f01379d8119c15d058d4c930e39c0f0a7f982e61
João Marcos <joaomarcos@frwk.com.br>
2024-04-02 19:27:27 -0300

    feat: add users, notes, controllers, routes, migrations, knex

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,81 @@
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(req, res) {
+  async create(req, res) {
     const { name, email, password } = req.body;
 
-    if (!name) {
-      throw new AppError("bruh");
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get(
+      `SELECT * FROM users WHERE email = (?)`,
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("This email is already in use");
     }
 
-    res.send({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      `INSERT INTO users (name, email, password) VALUES (?,?,?)`,
+      [name, email, hashedPassword]
+    );
+
+    return res.status(201).json();
+  }
+
+  async update(req, res) {
+    const { name, email, password, old_password } = req.body;
+    const { id } = req.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("User not found");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("This email is already in use");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "You need to enter the old password to change to a new password"
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("The old password is incorrect");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(
+      `UPDATE users SET 
+    name = ?,
+    email = ?,
+    password = ?,
+    updated_at = DATETIME('now')
+    WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
+
+    return res.json();
   }
 }
 
